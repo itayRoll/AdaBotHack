c02a4ed3370d95bc7dc6fe184a7466276d8102ae
Itay Roll <itroll@microsoft.com>
2018-07-23 17:28:09 +0300

    hello world

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=adabothack-Web-Deploy.pubxml -p:Password=aLiZnnvMYRsKN0SqRgtxmdt3WqzJHK0FyBzpuTy0Z8je5YZasvh9btu251Sb
+
