d530735ba9cc619916b6904776e491559e49e44a
MIDDLEEAST\Tomnof <tomnof@microsoft.com>
2018-07-24 12:22:12 +0300

    Query object

---
+++ Provider/Query.cs
@@ -0,0 +1,14 @@
+﻿namespace Provider
+{
+    public class Query
+    {
+        private string Age { get; set; }
+        private string Level { get; set; }
+        private string Domain { get; set; }
+        private string MediumType { get; set; }
+        private string Language { get; set; }
+        private string ProgramingLanguage { get; set; }
+        private string Duration { get; set; }
+        private string Price { get; set; }
+    }
+}
