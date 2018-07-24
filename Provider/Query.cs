b183566906a0c8acd12fb7e3954d9e35d32094ee
Itay Roll <itroll@microsoft.com>
2018-07-24 14:45:02 +0300

    Add structured conversation

--- Provider/Query.cs
+++ Provider/Query.cs
@@ -2,13 +2,32 @@
 {
     public class Query
     {
-        private string Age { get; set; }
+	    public Query(int age, string level, string domain, string mediumType, string language = "english", string programingLanguage = "", string duration = "", string price = "")
+	    {
+		    this.Age = age;
+		    this.Level = level;
+			this.Domain = domain;
+		    this.MediumType = mediumType;
+		    this.Language = language;
+		    this.ProgramingLanguage = programingLanguage;
+		    this.Duration = duration;
+		    this.Price = price;
+	    }
+
+        private int Age { get; set; }
+
         private string Level { get; set; }
-        private string Domain { get; set; }
-        private string MediumType { get; set; }
-        private string Language { get; set; }
-        private string ProgramingLanguage { get; set; }
-        private string Duration { get; set; }
-        private string Price { get; set; }
+
+	    private string Domain { get; set; }
+
+	    private string MediumType { get; set; }
+
+	    private string Language { get; set; }
+
+	    private string ProgramingLanguage { get; set; }
+
+	    private string Duration { get; set; }
+
+	    private string Price { get; set; }
     }
 }
