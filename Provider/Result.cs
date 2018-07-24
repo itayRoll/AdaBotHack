28e431bfe10e99fb20c998ae9d9f39fbefae947f
MIDDLEEAST\Tomnof <tomnof@microsoft.com>
2018-07-24 11:59:34 +0300

    Generate Proveider project & Result object

---
+++ Provider/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace Provider
+{
+    using Newtonsoft.Json;
+
+    public class Result
+    {
+        [JsonProperty("id")]
+        private string Id { get; set; }
+
+        [JsonProperty("displayName")]
+        private string DisplayName { get; set; }
+
+        [JsonProperty("description")]
+        private string Description { get; set; }
+
+        [JsonProperty("link")]
+        private string Link { get; set; }
+
+        [JsonProperty("image")]
+        private string Image { get; set; }
+
+    }
+}
