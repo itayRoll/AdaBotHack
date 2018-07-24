e6d50f9e59c56a6e47f6d8b2b5221947ec9b6648
MIDDLEEAST\Tomnof <tomnof@microsoft.com>
2018-07-24 17:13:05 +0300

    filter only filled optional fields

--- DataProvider/FileProvider.cs
+++ DataProvider/FileProvider.cs
@@ -12,20 +12,35 @@
             RepositoryEntitys entities = JsonConvert.DeserializeObject<RepositoryEntitys>(Resource.Repository);
 
             // Filter mendatory fields
-            List<RepositoryEntity> match = entities.Entities.Where(e =>
+            List<RepositoryEntity> mendatoryMatch = entities.Entities.Where(e =>
                 e.Level.ToString() == query.Level && 
                 e.Domain.ToString() == query.Domain &&
                 e.MediumType.ToString() == query.MediumType).ToList();
 
             // Filter optional fields
-            List<RepositoryEntity> optionalMatch = match.Where(e =>
-                e.Language == query.Language &&
-                e.ProgrammingLanguage == query.ProgrammingLanguage &&
-                e.Duration == query.Duration &&
-                e.ProgrammingLanguage == query.Price).ToList();
+            List<RepositoryEntity> optionalMatch = mendatoryMatch;
+            if (!string.IsNullOrEmpty(query.Language))
+            {
+                optionalMatch = optionalMatch.Where(e => e.Language == query.Language).ToList();
+            }
 
-            return optionalMatch.Any() ? optionalMatch.Select(a => a.BuildResult()).ToList() : 
-                match.Any() ? match.Select(a => a.BuildResult()).ToList() : new List<Result>();
+            if (!string.IsNullOrEmpty(query.ProgrammingLanguage))
+            {
+                optionalMatch = optionalMatch.Where(e => e.ProgrammingLanguage == query.ProgrammingLanguage).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(query.Duration))
+            {
+                optionalMatch = optionalMatch.Where(e => e.Duration == query.Duration).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(query.Price))
+            {
+                optionalMatch = optionalMatch.Where(e => e.Price == query.Price).ToList();
+            }
+
+            return optionalMatch.Any() ? optionalMatch.Select(a => a.BuildResult()).ToList() :
+                mendatoryMatch.Any() ? mendatoryMatch.Select(a => a.BuildResult()).ToList() : new List<Result>();
         }
     }
 }
