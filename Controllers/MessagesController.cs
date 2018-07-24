1139652df15897f35d77841335704af352c005da
Jonathan Geffen <jogeffen@microsoft.com>
2018-07-24 15:40:25 +0300

    Add LuisDialog

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
+    using global::SimpleEchoBot.Dialogs;
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
@@ -22,7 +24,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             // check if activity is of type message
             if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new EchoDialog());
+                await Conversation.SendAsync(activity, () => new AdaBotLuisDialog());
             }
             else
             {
