8a74eeadcb7e148c1c6e01c7ed8f7ecf34dfe1f5
MIDDLEEAST\Tomnof <tomnof@microsoft.com>
2018-07-25 12:08:10 +0300

    add greeting

--- Dialogs/AdaBotLuisDialog.cs
+++ Dialogs/AdaBotLuisDialog.cs
@@ -1,44 +1,53 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Builder.Luis.Models;
-using System;
-using System.Configuration;
-using System.Threading.Tasks;
-
-namespace SimpleEchoBot.Dialogs
+﻿namespace SimpleEchoBot.Dialogs
 {
+    using System;
     using System.Collections.Generic;
+    using System.Configuration;
     using System.Linq;
     using System.Text.RegularExpressions;
-
-    using Microsoft.Bot.Connector;
-
+    using System.Threading.Tasks;
     using DataProvider;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using Microsoft.Bot.Connector;
 
     [Serializable]
     public class AdaBotLuisDialog : LuisDialog<object>
     {
-        private string mediumType;
+        private static readonly List<LevelType> levels = Enum.GetValues(typeof(LevelType)).Cast<LevelType>().ToList();
+
+        private static readonly IEnumerable<DomainType> interests =
+            Enum.GetValues(typeof(DomainType)).Cast<DomainType>().ToList();
 
         private int age;
 
-        private LevelType level;
-
         private DomainType domain;
 
-        private static readonly List<LevelType> levels = Enum.GetValues(typeof(LevelType)).Cast<LevelType>().ToList();
-
-        private static readonly IEnumerable<DomainType> interests = Enum.GetValues(typeof(DomainType)).Cast<DomainType>().ToList();
+        private LevelType level;
+        private string mediumType;
 
         public AdaBotLuisDialog()
             : base(
                 new LuisService(
-                    model: new LuisModelAttribute(
+                    new LuisModelAttribute(
                         ConfigurationManager.AppSettings["LuisAppId"],
                         ConfigurationManager.AppSettings["LuisAPIKey"],
                         domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
         {
         }
+
+        public static string Greeting { get; } = @"Hi I’m AdaBot!
+                                                    \n
+                                                    I would love to provide you with some great content for programming.
+                                                    \n
+                                                    Let me know the Domain, Medium type, level and age of your child, so I can find something that will do.
+                                                    \n
+                                                    You can also provide the Language, Programming language and Duration.
+                                                    \n
+                                                    For example: “I want a workshop for mobile development for my 9 years old who is an intermediate”
+                                                    \n
+                                                    Or: “I'm looking for an inspirational video for my 9 years old who is a beginner”";
 
         [LuisIntent("None")]
         public async Task NoneIntent(IDialogContext context, LuisResult result)
@@ -50,7 +59,7 @@ namespace SimpleEchoBot.Dialogs
         [LuisIntent("Greet")]
         public async Task GreetIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Hello! And welcome to AdaBot!");
+            await context.PostAsync(Greeting);
             context.Wait(this.MessageReceived);
         }
 
@@ -64,16 +73,15 @@ namespace SimpleEchoBot.Dialogs
 
                 if (result.TryFindEntity("builtin.age", out inputAge))
                 {
-                    var match = Regex.Match(inputAge.Entity, "\\d+");
-                    this.age = Convert.ToInt32(match.Value);
+                    Match match = Regex.Match(inputAge.Entity, "\\d+");
+                    age = Convert.ToInt32(match.Value);
 
                     if (result.TryFindEntity("medium", out inputMedium))
                     {
-                        this.mediumType = inputMedium.Entity;
+                        mediumType = inputMedium.Entity;
 
                         PromptDialog.Choice(
-                            context,
-                            LevelSelectedAsync,
+                            context, this.LevelSelectedAsync,
                             levels,
                             "Great! What is your kid's level of knowledge?",
                             "Didn't get that!");
@@ -96,17 +104,16 @@ namespace SimpleEchoBot.Dialogs
 
         public async Task LevelSelectedAsync(IDialogContext context, IAwaitable<LevelType> argument)
         {
-            var selectedLevel = await argument;
+            LevelType selectedLevel = await argument;
 
             if (levels.Contains(selectedLevel))
             {
                 // valid level selected
-                this.level = selectedLevel;
+                level = selectedLevel;
                 PromptDialog.Choice(
-                    context,
-                    InterestSelectedAsync,
+                    context, this.InterestSelectedAsync,
                     interests,
-                    $"I found a lot of {this.mediumType} options for the {this.level} level. What is your kid interested in?",
+                    $"I found a lot of {mediumType} options for the {level} level. What is your kid interested in?",
                     "Didn't get that!");
             }
             else
@@ -117,32 +124,27 @@ namespace SimpleEchoBot.Dialogs
 
         public async Task InterestSelectedAsync(IDialogContext context, IAwaitable<DomainType> argument)
         {
-            var selectedInterest = await argument;
+            DomainType selectedInterest = await argument;
 
             if (interests.Contains(selectedInterest))
             {
                 // valid level selected
-                this.domain = selectedInterest;
+                domain = selectedInterest;
 
                 // get input from user
-                var query = new Query(this.age, this.level.ToString(), this.domain.ToString(), this.mediumType);
+                Query query = new Query(age, level.ToString(), domain.ToString(), mediumType);
 
-                var provider = new FileProvider();
+                FileProvider provider = new FileProvider();
                 // get results accourding to the input
-                var results = provider.GetResults(query);
-                
+                List<Result> results = provider.GetResults(query);
+
                 IMessageActivity reply;
                 if (results.Any())
-                {
-                    reply = GetReply(context, results.First());
-                }
+                    reply = this.GetReply(context, results.First());
                 else
-                {
-                    reply = GetCannedReply(context);
-                }
+                    reply = this.GetCannedReply(context);
 
                 await context.PostAsync(reply);
-
             }
             else
             {
@@ -150,19 +152,24 @@ namespace SimpleEchoBot.Dialogs
             }
 
             context.Wait(this.MessageReceived);
-        }       
+        }
 
         private IMessageActivity GetReply(IDialogContext context, Result suggestedResult)
         {
-            CardAction getStartedCardAction = new CardAction(ActionTypes.OpenUrl, "Get Started", value: suggestedResult.Link);
+            CardAction getStartedCardAction =
+                new CardAction(ActionTypes.OpenUrl, "Get Started", value: suggestedResult.Link);
             CardAction getMoreCardAction = new CardAction(ActionTypes.PostBack, "Get More", value: "GetMore");
 
-            var thumbnailCard = new ThumbnailCard
+            ThumbnailCard thumbnailCard = new ThumbnailCard
             {
                 Title = suggestedResult.DisplayName,
                 Text = suggestedResult.Description,
-                Images = new List<CardImage> { new CardImage("https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHuJWk0rfcAhWRCOwKHSZvCT4QjRx6BAgBEAU&url=http%3A%2F%2Fwww.edutechpost.com%2Fcodemonkey-coding-children%2F&psig=AOvVaw3VQGIaT364jlPrZWbZN5_S&ust=1532518456232233") },
-                Buttons = new List<CardAction> { getStartedCardAction, }
+                Images = new List<CardImage>
+                {
+                    new CardImage(
+                        "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHuJWk0rfcAhWRCOwKHSZvCT4QjRx6BAgBEAU&url=http%3A%2F%2Fwww.edutechpost.com%2Fcodemonkey-coding-children%2F&psig=AOvVaw3VQGIaT364jlPrZWbZN5_S&ust=1532518456232233")
+                },
+                Buttons = new List<CardAction> {getStartedCardAction}
             };
 
             IMessageActivity reply = context.MakeMessage();
@@ -173,21 +180,26 @@ namespace SimpleEchoBot.Dialogs
 
         private IMessageActivity GetCannedReply(IDialogContext context)
         {
-            Result suggestedResult = new Result()
+            Result suggestedResult = new Result
             {
                 DisplayName = "Code Monkey- Python chatbot",
                 Link = "https://www.playcodemonkey.com/python-chatbot/",
                 Description = "Create a chatbot using python!"
             };
 
-            CardAction getStartedCardAction = new CardAction(ActionTypes.OpenUrl, "Get Started", value: suggestedResult.Link);
+            CardAction getStartedCardAction =
+                new CardAction(ActionTypes.OpenUrl, "Get Started", value: suggestedResult.Link);
 
-            var thumbnailCard = new ThumbnailCard
+            ThumbnailCard thumbnailCard = new ThumbnailCard
             {
                 Title = suggestedResult.DisplayName,
                 Text = suggestedResult.Description,
-                Images = new List<CardImage> { new CardImage("https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHuJWk0rfcAhWRCOwKHSZvCT4QjRx6BAgBEAU&url=http%3A%2F%2Fwww.edutechpost.com%2Fcodemonkey-coding-children%2F&psig=AOvVaw3VQGIaT364jlPrZWbZN5_S&ust=1532518456232233") },
-                Buttons = new List<CardAction> { getStartedCardAction }
+                Images = new List<CardImage>
+                {
+                    new CardImage(
+                        "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHuJWk0rfcAhWRCOwKHSZvCT4QjRx6BAgBEAU&url=http%3A%2F%2Fwww.edutechpost.com%2Fcodemonkey-coding-children%2F&psig=AOvVaw3VQGIaT364jlPrZWbZN5_S&ust=1532518456232233")
+                },
+                Buttons = new List<CardAction> {getStartedCardAction}
             };
 
             IMessageActivity reply = context.MakeMessage();
